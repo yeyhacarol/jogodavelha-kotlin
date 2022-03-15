3b2bfa23e89afb0252e2a86879624a6e5663f0fd
CAROLINA VITÓRIA CHAVES SILVA <21276093@CFP127ALUNO.LOCAL>
2022-03-15 17:00:19 -0300

    criando página inicial

--- app/src/main/java/com/senai/jogodavelha/SplashActivity.kt
+++ app/src/main/java/com/senai/jogodavelha/SplashActivity.kt
@@ -1,11 +1,23 @@
 package com.senai.jogodavelha
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
+import kotlin.concurrent.schedule
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Timer().schedule(3000){abrirMainActivity()}
+    }
+
+    private fun abrirMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
+
